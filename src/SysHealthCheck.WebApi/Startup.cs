7217346e9610d38a57a0931a6ae7e1014e8169b8
Worawut Ouncharoen <worawut.oun@gmail.com>
2021-04-25 13:45:46 +0700

    (WO) add ui

--- src/SysHealthCheck.WebApi/Startup.cs
+++ src/SysHealthCheck.WebApi/Startup.cs
@@ -11,7 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SysHealthCheck.WebApp
+namespace SysHealthCheck.WebApi
 {
     public class Startup
     {
@@ -19,9 +19,9 @@ namespace SysHealthCheck.WebApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services
-            .AddHealthChecksUI()
-            .AddInMemoryStorage();
+
+            services.AddHealthChecks()
+                .AddCheck("AlwaysHealthy", () => HealthCheckResult.Healthy());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,12 +36,10 @@ namespace SysHealthCheck.WebApp
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapHealthChecksUI();                
-
-                endpoints.MapGet("/", context =>
+                endpoints.MapHealthChecks("/", new HealthCheckOptions()
                 {
-                    context.Response.Redirect("/healthchecks-ui", permanent: false);
-                    return Task.FromResult(0);
+                    Predicate = _ => true,
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
                 });
             });
         }
